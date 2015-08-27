5bcb3bda645faa65da0e9657020b45ef6831d678
Hillel Coren <hillelcoren@gmail.com>
2015-08-27 20:23:31 +0300

    Added groupby and sortby

--- data.js
+++ data.js
@@ -1,51 +1,89 @@
+var candidateList = [
+    'Bernie Sanders',
+    'Hillary Clinton',
+    'Jeb Bush',
+    'Ted Cruz',
+    'Marco Rubio',
+    'Scott Walker',
+];
+
+var groupByFields = [
+    'Candidate',
+    'Super PAC',
+    'Industry',
+];
+
+var sortByFields = [
+    'Super PAC',
+    'Candidate',
+    'Amount',
+    'Industry',
+];
+
 var dataset = [
     {
         "name": "Steven Spielberg",
         "pac": "Priorities USA Action",
         "candidate": "Hillary Clinton",
-        "amount": 1
+        "amount": 1,
+        "industry": "Media",
+        "link1": "https://en.wikipedia.org/wiki/Steven_Spielberg"
     },
     {
         "name": "George Soros",
         "pac": "Priorities USA Action",
         "candidate": "Hillary Clinton",
-        "amount": 1
+        "amount": 1,
+        "industry": "Finance",
+        "link1": "https://en.wikipedia.org/wiki/George_Soros",
+        "link2": "https://www.opensecrets.org/outsidespending/donor_detail.php?cycle=2014&id=U0000000364&type=I&super=S&name=Soros%2C+George"
     },
     {
         "name": "Jeffrey Katzenberg",
         "pac": "Priorities USA Action",
         "candidate": "Hillary Clinton",
-        "amount": 1
+        "amount": 1,
+        "industry": "Media",
+        "link1": "https://en.wikipedia.org/wiki/Jeffrey_Katzenberg"
     },
     {
         "name": "Herbert M. Sandler",
         "pac": "Priorities USA Action",
         "candidate": "Hillary Clinton",
-        "amount": 1
+        "amount": 1,
+        "industry": "Finance",
+        "link1": "https://en.wikipedia.org/wiki/Herbert_Sandler"
     },
     {
         "name": "Haim Saban",
         "pac": "Priorities USA Action",
         "candidate": "Hillary Clinton",
-        "amount": 1
+        "amount": 1,
+        "industry": "Media",
+        "link1": "https://en.wikipedia.org/wiki/Haim_Saban"
     },
     {
         "name": "Cheryl Saban",
         "pac": "Priorities USA Action",
         "candidate": "Hillary Clinton",
-        "amount": 1
+        "amount": 1,
+        "industry": "Media",
+        "link1": "https://en.wikipedia.org/wiki/Cheryl_Saban"
     },
     {
         "name": "Donald Sussman",
         "pac": "Priorities USA Action",
         "candidate": "Hillary Clinton",
-        "amount": 1
+        "amount": 1,
+        "industry": "Finance",
+        "link1": "https://en.wikipedia.org/wiki/Donald_Sussman"
     },
     {
         "name": "Fair Share Action",
         "pac": "Priorities USA Action",
         "candidate": "Hillary Clinton",
-        "amount": 1
+        "amount": 1,
+        "link1": "https://en.wikipedia.org/wiki/Fair_Share_Action"
     },
     {
         "name": "UA Political Education Committee",
@@ -63,19 +101,22 @@ var dataset = [
         "name": "Hillary For America",
         "pac": "Correct the Record",
         "candidate": "Hillary Clinton",
-        "amount": 0.28
+        "amount": 0.28,
+        "link1": "https://en.wikipedia.org/wiki/Hillary_Clinton_presidential_campaign,_2016"
     },
     {
         "name": "Pat A Stryker",
         "pac": "Correct the Record",
         "candidate": "Hillary Clinton",
-        "amount": 0.25
+        "amount": 0.25,
+        "link1": "https://en.wikipedia.org/wiki/Pat_Stryker"
     },
     {
         "name": "Stephen M Silberstein",
         "pac": "Correct the Record",
         "candidate": "Hillary Clinton",
-        "amount": 0.2
+        "amount": 0.2,
+        "link1": "http://rainmaker.apps.cironline.org/donors/stephen-m-silberstein/"
     },
     {
         "name": "Other Donors",
@@ -117,7 +158,9 @@ var dataset = [
         "name": "John W. Childs",
         "pac": "Keep the Promise PAC",
         "candidate": "Ted Cruz",
-        "amount": 0.25
+        "amount": 0.25,
+        "industry": "Finance",
+        "link1": "https://en.wikipedia.org/wiki/John_W._Childs"
     },
     {
         "name": "Thomas Patrick",
@@ -135,7 +178,9 @@ var dataset = [
         "name": "Robert Mercer",
         "pac": "Keep the Promise I",
         "candidate": "Ted Cruz",
-        "amount": 11
+        "amount": 11,
+        "industry": "Finance",
+        "link1": "https://en.wikipedia.org/wiki/Robert_Mercer_(businessman)"
     },
     {
         "name": "William Lee Hanley, Jr.",
@@ -147,43 +192,57 @@ var dataset = [
         "name": "Toby Neugebauer",
         "pac": "Keep the Promise II",
         "candidate": "Ted Cruz",
-        "amount": 10
+        "amount": 10,
+        "industry": "Finance",
+        "link1": "https://en.wikipedia.org/wiki/Toby_Neugebauer"
     },
     {
         "name": "Farris Wilks",
         "pac": "Keep the Promise III",
         "candidate": "Ted Cruz",
-        "amount": 5
+        "amount": 5,
+        "industry": "Fracking",
+        "link1": "https://en.wikipedia.org/wiki/Dan_and_Farris_Wilks"
     },
     {
         "name": "Jo Ann Wilks",
         "pac": "Keep the Promise III",
         "candidate": "Ted Cruz",
-        "amount": 5
+        "amount": 5,
+        "industry": "Fracking",
+        "link1": "https://en.wikipedia.org/wiki/Dan_and_Farris_Wilks"
     },
     {
         "name": "Daniel Wilks",
         "pac": "Keep the Promise III",
         "candidate": "Ted Cruz",
-        "amount": 2.5
+        "amount": 2.5,
+        "industry": "Fracking",
+        "link1": "https://en.wikipedia.org/wiki/Dan_and_Farris_Wilks"
     },
     {
         "name": "Staci Wilks",
         "pac": "Keep the Promise III",
         "candidate": "Ted Cruz",
-        "amount": 2.5
+        "amount": 2.5,
+        "industry": "Fracking",
+        "link1": "https://en.wikipedia.org/wiki/Dan_and_Farris_Wilks"
     },
     {
         "name": "Norman Braman",
         "pac": "Conservative Solutions PAC",
         "candidate": "Marco Rubio",
-        "amount": 5
+        "amount": 5,
+        "industry": "Business",
+        "link1": "https://en.wikipedia.org/wiki/Norman_Braman"
     },
     {
         "name": "Lawrence J. Ellison",
         "pac": "Conservative Solutions PAC",
         "candidate": "Marco Rubio",
-        "amount": 3
+        "amount": 3,
+        "industry": "Business",
+        "link1": "https://en.wikipedia.org/wiki/Larry_Ellison"
     },
     {
         "name": "Besilu Stables, LLC",
@@ -207,7 +266,9 @@ var dataset = [
         "name": "Diane Hendricks",
         "pac": "Unintimidated PAC",
         "candidate": "Scott Walker",
-        "amount": 5
+        "amount": 5,
+        "industry": "Business",
+        "link1": "https://en.wikipedia.org/wiki/Diane_Hendricks"
     },
     {
         "name": "Marlene Ricketts",
@@ -219,7 +280,9 @@ var dataset = [
         "name": "Richard Uihlein",
         "pac": "Unintimidated PAC",
         "candidate": "Scott Walker",
-        "amount": 1.75
+        "amount": 1.75,
+        "industry": "Business",
+        "link1": "https://en.wikipedia.org/wiki/Richard_Uihlein"
     },
     {
         "name": "Other Donors",
@@ -237,7 +300,9 @@ var dataset = [
         "name": "Access Industries",
         "pac": "Our American Revival",
         "candidate": "Scott Walker",
-        "amount": 0.25
+        "amount": 0.25,
+        "industry": "Business",
+        "link1": "https://en.wikipedia.org/wiki/Access_Industries"
     },
     {
         "name": "Jeanne Sinquefield",
@@ -249,12 +314,14 @@ var dataset = [
         "name": "Richard Uihlein",
         "pac": "Our American Revival",
         "candidate": "Scott Walker",
-        "amount": 0.2
+        "amount": 0.2,
+        "industry": "Business",
+        "link1": "https://en.wikipedia.org/wiki/Richard_Uihlein"
     },
     {
         "name": "Other Donors",
         "pac": "Our American Revival",
         "candidate": "Scott Walker",
         "amount": 5.01
-    }
+    },
 ];
--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-var candidates = [
-    'Bernie Sanders',
-    'Hillary Clinton',
-    'Jeb Bush',
-    'Ted Cruz',
-    'Marco Rubio',
-    'Scott Walker',
-]
+// Pre-process the data
+var labelData = [];
+var settings = {
+    groupBy: 'candidate',
+    sortBy: 'amount'
+};
 
-var totals = [
-    0,
-    17.09,
-    103.17,
-    37.83,
-    16.06,
-    26.23,
-]
-var max = 103.17;
-
-// Pre-process the data filling in the zeros
-var counter = 0;
+var pacList = [];
+var industryList = [];
 for (var i=0; i<dataset.length; i++) {
-    var set = dataset[i];    
-    var candidateId = candidates.indexOf(set.candidate);
-    if (candidateId < 0) {
-        console.log('Error: unknown candidate %s', set.candidate);
+    var set = dataset[i];
+    
+    // Determine the list of PACs and industries
+    if (pacList.indexOf(set.pac) === -1) {
+        pacList.push(set.pac);
     }
-    dataset[i].values = [{
-        "id": counter++,
-        "x": candidateId,
-        "y": set.amount,
-    }]
-    for (var j=0; j<candidates.length; j++) {
-        var hasCandidate = false;
-        for (var k=0; k<set.values.length; k++) {
-            var donation = set.values[k];
-            if (donation.x == j) {
-                hasCandidate = true;
-                break;
-            }
-        }
-        if (!hasCandidate) {
-            dataset[i]["values"].splice(j, 0, {"x":j, "y":0, "id": 0});
-        }
+    if (industryList.indexOf(set.industry) === -1) {
+        industryList.push(set.industry);
     }
+
+    // Fill in the blanks
+    if (!set.industry) {
+        dataset[i].industry = 'Unknown';
+    }
+
+    // Process external site links
+    for (var j=1; j<=2; j++) {
+        var key = 'link' + j;        
+        if (set[key]) {            
+            var url = set[key];
+            var logo = '';
+            var title = '';
+            if (url.indexOf('wikipedia.org') > 0) {
+                logo = 'wiki.png';
+                title = 'Wikipedia';
+            } else if (url.indexOf('opensecrets.org') > 0) {
+                logo = 'secrets.ico';
+                title = 'OpenSecrets';
+            } else if (url.indexOf('cironline.org') > 0) {
+                logo = 'cironline.png';                
+                title = 'Center for Investigative Reporting';
+            }            
+            dataset[i][key] = '<a href="' + url + '" target="_blank" title="' + title + '"><img src="logos/' + logo + '"/></a>';            
+        }
+    }    
 }
-//console.log(JSON.stringify(dataset));
+pacList.sort();
+industryList.sort();
+
 // Specify the chart area and dimensions
 var chart = d3.select(".chart")
     .attr("width", 100)
@@ -59,192 +61,145 @@ var colors = colors = d3.scale.category20c();
 var stack = d3.layout.stack()
     .values(function(d) { return d.values; });
 
-var scale = d3.scale.linear()
-    .domain([0, max])
-    .range([0, chartWidth - labelWidth]);
-
 var div = d3.select("body")
     .append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);  
 
-var donation = chart.selectAll("g.donation")
-      .data(stack(dataset))
-      .enter().append("svg:g")
-      .style("fill", function(d, i) { return "#ea504e"; })
-      .style("stroke", function(d, i) { return 10; });
-
-var group = donation.selectAll("rect")
-      .data(function(d){return d.values;})
-      .enter();
-
-var rect = group.append("svg:rect")
-      .filter(function(d) { return d.y > 0 })
-      .attr("class", "donation")
-      .attr("x", function(d) { return labelWidth + scale(d.y0); })
-      .attr("y", function(d) { return d.x * (barWidth + barGap); })
-      .attr("fill", function(d,i) { return colors(d.id) })
-      .attr("height", barWidth)
-      .attr("width", function(d) { return scale(d.y); })
-      .on("mouseover", function(d) {
-        div.transition()
-            .duration(300)  
-            .style("opacity", .9);        
-        div.html('<div class="name">' + truncate(dataset[d.id].name, 25) + '</div>' +
-                '<div class="pac">' + dataset[d.id].pac + '</div>' +
-                '<div class="amount">$' + dataset[d.id].amount + ' Million</div>'
-            )  
-            .style("left", ($('.chart').offset().left + labelWidth + 4 + scale(d.y0)) + "px")
-            .style("top", $('.chart').offset().top + (d.x * (barWidth + barGap)) + "px");        
-      });
-
-chart.selectAll("g.candidate-name")
-    .data(candidates)
-    .enter().append("text")
-    .attr("class", "candidate-name")
-    .attr("x", 0)
-    .attr("y", function(d, i) { return (i * (barWidth + barGap) + 35) })
-    .text(function(d, i) { return candidates[i]  });
-
-chart.selectAll("g.candidate-amount")
-    .data(candidates)
-    .enter().append("text")
-    .attr("class", "candidate-amount")
-    .attr("x", 0)
-    .attr("y", function(d, i) { return (i * (barWidth + barGap) + 65) })
-    .text(function(d, i) { return '$' + (+totals[i] || 0).toFixed(2) + ' Million' });
-
-
-/*!
-* classie - class helper functions
-* from bonzo https://github.com/ded/bonzo
-* 
-* classie.has( elem, 'my-class' ) -> true/false
-* classie.add( elem, 'my-new-class' )
-* classie.remove( elem, 'my-unwanted-class' )
-* classie.toggle( elem, 'my-class' )
-*/
-
-/*jshint browser: true, strict: true, undef: true */
-/*global define: false */
-
-( function( window ) {
-
-    'use strict';
-
-// class helper functions from bonzo https://github.com/ded/bonzo
-
-function classReg( className ) {
-    return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-}
-
-// classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
-var hasClass, addClass, removeClass;
-
-if ( 'classList' in document.documentElement ) {
-    hasClass = function( elem, c ) {
-        return elem.classList.contains( c );
-    };
-    addClass = function( elem, c ) {
-        elem.classList.add( c );
-    };
-    removeClass = function( elem, c ) {
-        elem.classList.remove( c );
-    };
-}
-else {
-    hasClass = function( elem, c ) {
-        return classReg( c ).test( elem.className );
-    };
-    addClass = function( elem, c ) {
-        if ( !hasClass( elem, c ) ) {
-            elem.className = elem.className + ' ' + c;
-        }
-    };
-    removeClass = function( elem, c ) {
-        elem.className = elem.className.replace( classReg( c ), ' ' );
-    };
-}
-
-function toggleClass( elem, c ) {
-    var fn = hasClass( elem, c ) ? removeClass : addClass;
-    fn( elem, c );
-}
-
-var classie = {
-// full names
-hasClass: hasClass,
-addClass: addClass,
-removeClass: removeClass,
-toggleClass: toggleClass,
-// short names
-has: hasClass,
-add: addClass,
-remove: removeClass,
-toggle: toggleClass
+function drawChart() {
+    //console.log('== Draw chart - group:%s sort:%s', settings.groupBy, settings.sortBy);
+    d3.selectAll("svg > *").remove();
+    labelData = processData();
+    drawLabels();
+    drawRects();    
+    $('.chart').height((labelData.length * (barGap + barWidth) + 20));
 };
 
-// transport
-if ( typeof define === 'function' && define.amd ) {
-// AMD
-define( classie );
-} else {
-// browser global
-window.classie = classie;
+function drawRects() {
+    var scale = d3.scale.linear()
+        .domain([0, getMax()])
+        .range([0, chartWidth - labelWidth]);
+
+    var donation = chart.selectAll("g")
+          .data(dataset).enter()
+        .append("svg:g")
+          .style("fill", function(d, i) { return "#ea504e"; })
+          .style("stroke", function(d, i) { return 10; })
+        .append("svg:rect")
+          .attr("class", "donation")
+          .attr("x", function(d) { return labelWidth + scale(d.prevAmount); })
+          .attr("y", function(d) { return findRow(d) * (barWidth + barGap); })
+          .attr("fill", function(d,i) { return colors(d.id); })
+          .attr("height", barWidth)
+          .attr("width", function(d) { return scale(d.amount || 0); })
+          .on("mouseover", function(d) {
+            div.transition()
+                .duration(300)  
+                .style("opacity", .9);        
+            div.html('<div class="pull-right">' + (d.link1 || '') + (d.link2 || '') + '</div>' +
+                    '<div class="name">' + truncate(d.name, 25) + '</div>' +
+                    '<div class="pac">' + d.pac + '</div>' +
+                    '<div class="industry pull-right">' + d.industry + '</div>' +
+                    '<div class="amount">$' + d.amount + ' Million</div>'
+                )  
+                .style("left", ($('.chart').offset().left + labelWidth + scale(d.prevAmount)) + "px")
+                .style("top", $('.chart').offset().top + (findRow(d) * (barWidth + barGap)) + "px");        
+          });
 }
 
-})( window );
 
-
-/**
-* cbpAnimatedHeader.js v1.0.0
-* http://www.codrops.com
-*
-* Licensed under the MIT license.
-* http://www.opensource.org/licenses/mit-license.php
-* 
-* Copyright 2013, Codrops
-* http://www.codrops.com
-*/
-var cbpAnimatedHeader = (function() {
-
-    var docElem = document.documentElement,
-    header = document.querySelector( '.navbar-fixed-top' ),
-    didScroll = false,
-    changeHeaderOn = 300;
-
-    function init() {
-        window.addEventListener( 'scroll', function( event ) {
-            if( !didScroll ) {
-                didScroll = true;
-                setTimeout( scrollPage, 250 );
-            }
-        }, false );
-    }
-
-    function scrollPage() {
-        var sy = scrollY();
-        if ( sy >= changeHeaderOn ) {
-            classie.add( header, 'navbar-shrink' );
+function findRow(set) {
+    for (var i=0; i<labelData.length; i++) {
+        var data = labelData[i];
+        var groupBy = settings.groupBy;
+        var key = (groupBy == 'Super PAC' ? 'pac' : groupBy.toLowerCase());
+        if (data.name == set[key]) {
+            return i;
         }
-        else {
-            classie.remove( header, 'navbar-shrink' );
+    }
+    return -1;
+}
+
+function getMax() {
+    var max = 0;
+    for (var i=0; i<labelData.length; i++) {
+        var data = labelData[i];
+        max = Math.max(max, data.amount);
+    }
+    return max;
+}
+
+function drawLabels() {
+    chart.selectAll("g.candidate-name")
+        .data(labelData)
+        .enter().append("text")
+        .attr("class", "candidate-name")
+        .attr("x", 0)
+        .attr("y", function(d, i) { return (i * (barWidth + barGap) + 35) })
+        .text(function(d, i) { return d.name  });
+
+    chart.selectAll("g.candidate-amount")
+        .data(labelData)
+        .enter().append("text")
+        .attr("class", "candidate-amount")
+        .attr("x", 0)
+        .attr("y", function(d, i) { return (i * (barWidth + barGap) + 65) })
+        .text(function(d, i) { return '$' + (d.amount ? d.amount.toFixed(2) : 0) + ' Million' });    
+}
+
+function compareValues(a, b, field, direction) {
+    var value1 = a[field] || '';
+    var value2 = b[field] || '';
+
+    if (typeof value1 === 'string' 
+        && typeof value2 === 'string') {
+        value1 = value1.toLowerCase();
+        value2 = value2.toLowerCase();
+        return value1.localeCompare(value2);
+    } else {
+        return direction == 'asc' ? value1 - value2 : value2 - value1;
+    }
+}
+
+function processData() {
+    dataset.sort(function(a, b) {
+        if (compareValues(a, b, settings.groupBy) === 0) {
+            return compareValues(a, b, settings.sortBy, 'asc');
+        } else {
+            return compareValues(a, b, settings.groupBy, 'asc');
         }
-        didScroll = false;        
+    });
+
+    var counter = 0;
+    var lastValue = 0;
+    var lastGroup = false;
+    var data = [];
+    if (settings.groupBy == 'candidate') {
+        data.push({'name': 'Bernie Sanders', 'amount': 0});
     }
+    for (var i=0; i<dataset.length; i++) {
+        var set = dataset[i];
+        if (lastGroup != set[settings.groupBy]) {
+            lastValue = 0;
+        }
 
-    function scrollY() {
-        return window.pageYOffset || docElem.scrollTop;
+        // calculate the totals
+        var groupBy = set[settings.groupBy];
+        if (!lastValue) {
+            data.push({'name': groupBy, 'amount': 0});
+        };
+        data[data.length-1].amount += set.amount;
+        dataset[i].id = counter++;
+        dataset[i].prevAmount = lastValue;
+        lastValue += set.amount;
+        lastGroup = set[settings.groupBy];
     }
-
-    init();
-
-})();
-
-function truncate(string, length){
-   if (string.length > length)
-      return string.substring(0,length)+'...';
-   else
-      return string;
-};
+    data.sort(function(a, b) {
+        if (settings.groupBy === 'candidate') {
+            if (a.name == 'Bernie Sanders') return -1;
+            if (b.name == 'Bernie Sanders') return 1;
+        }
+        return compareValues(a, b, 'amount', 'desc');
+    });
+    return data;
+}
